9eb317c94ffcc11f583496be9301cf9a9dc821f6
Carlos Castañeda <dario20049@gmail.com>
2025-06-13 00:38:48 +0900

    jenkins war configuration

--- src/main/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplication.java
+++ src/main/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplication.java
@@ -2,12 +2,22 @@ package dev.dario.food.ordering.system.jenkinscicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsCiCdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsCiCdApplication.class, args);
-	}
+
+  @GetMapping("/greetings/{name}")
+  public String greetings(@PathVariable String name){
+    return "Hello " + name + " CI/CD demo";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenkinsCiCdApplication.class, args);
+  }
 
 }
--- src/test/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplicationTest.java
+++ src/test/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplicationTest.java
@@ -1,4 +1,30 @@
+package dev.dario.food.ordering.system.jenkinscicd;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
 class JenkinsCiCdApplicationTest {
-  
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testGreetingsEndpoint() throws Exception {
+    String name = "Dario Dario!";
+
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/greetings/{name}", name))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Hello " + name + " CI/CD demo"));
+  }
+
+
 }
