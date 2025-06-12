d8749c78fa6ffc154a15641a844e2928d4632aec
Carlos Castañeda <dario20049@gmail.com>
2025-06-12 15:55:16 +0900

    Initial commit

---
+++ src/main/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplication.java
@@ -0,0 +1,13 @@
+package dev.dario.food.ordering.system.jenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dario/food/ordering/system/jenkinscicd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dario.food.ordering.system.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
